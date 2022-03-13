1ecc1800ec6017f5184ab3e5dd65e4567c78ec8c
Ives Rodrigues <88946051+ivesrodrigues@users.noreply.github.com>
2022-03-13 16:57:29 +0000

    Desenvolvendo questão 1.

---
+++ ExerciciosIniciais/q1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+  
+  printf("Nome: Ives dos Anjos Seixas Rodrigues.");
+  printf("\nMatrícula: 20201160009.");
+  printf("\nSemestre de ingresso: 2020.1\n");
+
+  return 0;
+}
